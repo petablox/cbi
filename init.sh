036a18576656b9ae9d89ba29e3e4a66b8e25cfbb
Kihong Heo <kheo@cis.upenn.edu>
2017-12-17 21:01:52 +0000

    update

--- init.sh
+++ init.sh
@@ -5,4 +5,5 @@ export SAMPLER_EXIT_SIGNAL=0
 export SAMPLER_EXIT_STATUS=0
 export CBI_HOME=`pwd`
 export PATH="$CBI_HOME/bin/:$PATH"
+export LIBRARY_PATH="$CBI_HOME/sampler-1.6.2/driver/lib/:$LIBRARY_PATH"
 export LD_LIBRARY_PATH="$CBI_HOME/sampler-1.6.2/driver/lib/:$LD_LIBRARY_PATH"
